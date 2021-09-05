eb06d3640ed52264fabca015bc01b4a52f4d241a
nguyenthuphuong <Phuongnyo98@gmail.com>
2021-09-05 13:56:02 +0700

    [NTPhuong] Create tables & Insert data for tables

--- NguyenThuPhuong/phuong-app/database/migrations/2021_09_05_053053_update_products.php
+++ NguyenThuPhuong/phuong-app/database/migrations/2021_09_05_053053_update_products.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class UpdateProducts extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::table('products', function (Blueprint $table) {
+            $table->float('price')->nullable()->change();
+            $table->integer('sale_off')->default(0);
+            $table->bigInteger('user_id')->after('id'); 
+            $table->bigInteger('category_id')->after('id');
         });
     }
 
@@ -27,6 +28,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        //
     }
 }
