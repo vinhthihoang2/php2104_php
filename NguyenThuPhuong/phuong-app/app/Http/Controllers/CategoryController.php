74bf0efa12cb59962f0dd54cb33134a317cfb76d
nguyenthuphuong <Phuongnyo98@gmail.com>
2021-09-17 00:01:37 +0700

    [NTPhuong] - Eloquent Relationship

--- NguyenThuPhuong/phuong-app/app/Http/Controllers/CategoryController.php
+++ NguyenThuPhuong/phuong-app/app/Http/Controllers/CategoryController.php
@@ -21,7 +21,7 @@ class CategoryController extends Controller
     {
         $category = $this->categoryModel->findOrFail($id);
 
-        $products = $this->productModel
+        $products = $category->products()
             ->where('category_id', $category->id)
             ->where('is_public', config('product.public'))
             ->orderBy('sale_off', 'DESC')
