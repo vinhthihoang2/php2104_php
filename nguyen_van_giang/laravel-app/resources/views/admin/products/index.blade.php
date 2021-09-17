05acd678fc69f2035f07e8c98f65f9a1b2ed8984
giangnv91 <giangnv91@gmail.com>
2021-09-17 22:13:54 +0700

    [GiangNV] Controller, admin layout

---
+++ nguyen_van_giang/laravel-app/resources/views/admin/products/index.blade.php
@@ -0,0 +1,53 @@
+<x-admin-layout>
+  <div class="card">
+    <div class="card-header">
+      <h3 class="card-title">Bordered Table</h3>
+    </div>
+    <!-- /.card-header -->
+    <div class="card-body">
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th style="width: 10px">#</th>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Progress</th>
+            <th style="width: 40px">Label</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach ($products as $product)
+            <tr>
+              <td>{{ $loop->index + 1 }}.</td>
+              <td>
+                <a href="{{ route('admin.products.show', ['product' => $product->id]) }}">
+                  {{ $product->name }}
+                </a>
+              </td>
+              <td>
+                <a href="{{ route('category.show', ['id' => $product->category->id]) }}">
+                  {{ $product->category->name }}
+                </a>
+              </td>
+              <td>
+                <div class="progress progress-xs">
+                  <div class="progress-bar progress-bar-danger" style="width: 55%"></div>
+                </div>
+              </td>
+              <td>
+                <a href="{{ route('product.show', ['id' => $product->id]) }}" class="btn btn-block btn-info btn-xs" target="_blank">
+                  Demo
+                </a>
+              </td>
+            </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+    <!-- /.card-body -->
+
+    {{ $products->links('partials.admin-paginate') }}
+
+    </div>
+  </div>
+</x-admin-layout>
